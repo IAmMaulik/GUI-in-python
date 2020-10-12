2afae80f62656f5198e5679a8b7af52419be0ddb
IAmMaulik <71862933+IAmMaulik@users.noreply.github.com>
2020-10-12 12:31:53 +0530

    Add files via upload

--- MessageBox.py
+++ MessageBox.py
@@ -1,8 +1,13 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 
 def BuClick():
     print("UserName: {}, Password: {}".format(etUserName.get(), etPassword.get()))
+    if etUserName.get() == "admin" and etPassword.get() == "1234":
+        messagebox.showinfo(title="Login Info", message="Welcome to the app")
+    else:
+        messagebox.showinfo(title="Wrong UserName or Password", message="Access Denied")
 
 def ShowHide():
     etPassword.config(show="")
